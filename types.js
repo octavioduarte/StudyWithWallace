c70be5f3845a89aa7581d44d29d1f7e219dd2623
octavioduarte <octavioduarte2105@gmail.com>
2021-09-15 23:02:53 -0300

    text about types in javascript

---
+++ types.js
@@ -0,0 +1,10 @@
+/* 
+ Javascript  não é uma linguagem fortemente tipada, por tanto é necessário em alguns
+ casos realizar a conversão de algumas informações, para que as operações funcionem com êxito
+*/
+
+const n1 = "2"
+const n2 = "3"
+
+console.log(parseInt(n1) + parseInt(n2) )
+
